192876686e32bb4f7f9d032b299f7a477b3f96b2
ochab.mat@gmail.com <ochab.mat@gmail.com>
2023-02-22 17:00:57 +0100

    Initial commit

---
+++ src/main/java/com/analitiker/arctos/arctosjoinerpublic/ArctosJoinerPublicApplication.java
@@ -0,0 +1,13 @@
+package com.analitiker.arctos.arctosjoinerpublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArctosJoinerPublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArctosJoinerPublicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analitiker/arctos/arctosjoinerpublic/ArctosJoinerPublicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analitiker.arctos.arctosjoinerpublic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArctosJoinerPublicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
